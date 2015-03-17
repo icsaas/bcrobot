b11b558b184ebbe8e643d9115730648dc4eec91a
matrixorz <matrix56.ch@gmail.com>
2015-03-17 23:42:08 +0800

    Message admin

--- weixin/admin.py
+++ weixin/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from weixin.models import  User,New
+from weixin.models import  User,New,Message
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(New)
+admin.site.register(Message)
